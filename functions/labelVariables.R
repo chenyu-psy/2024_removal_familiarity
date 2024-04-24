0eb5a45566e08cafa5c0cd82ba5186843933dbd9
chenyu-psy <chenyu.li.hz@gmail.com>
2024-04-24 20:55:22 +0000

    update figures

--- functions/labelVariables.R
+++ functions/labelVariables.R
@@ -2,14 +2,43 @@
 
 labelCondition <- function(.data) {
   
-  results <- .data %>% 
-    mutate(
-      condition = factor(
-        condition,
-        levels = c("cn","kr"),
-        labels = c("Familiar (Chinese)", "Unfamiliar (Korean)")
-      ))
+  results <- .data
   
+  if ("condition" %in% names(.data)) {
+    results <- results %>% 
+      mutate(
+        condition = factor(
+          condition,
+          levels = c(
+            "cn","kr",
+            "Familiar (Chinese)", "Unfamiliar (Korean)"),
+          labels = c(
+            "Familiar (Chinese)", "Unfamiliar (Korean)",
+            "Familiar (Chinese)", "Unfamiliar (Korean)")
+        ))
+  }
+  
+  if ("itemType" %in% names(.data)) {
+    results <- results %>% 
+      mutate(
+        itemType = factor(
+          itemType,
+          levels = c("original","new"),
+          labels = c("Original items", "New items")
+        )
+      )
+  }
+  
+  if ("recallPos" %in% names(.data)) {
+    results <- results %>% 
+      mutate(
+        recallPos = factor(
+          recallPos,
+          levels = c("original","other"),
+          labels = c("Original position", "Other positions")
+        )
+      )
+  }
   
   return (results)
-} 
+}
