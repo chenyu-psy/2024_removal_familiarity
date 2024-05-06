61ccb92153987866c4a8b513ac4d9631ff0129a1
Chenyu <57704488+chenyu-psy@users.noreply.github.com>
2024-05-06 13:21:07 +0200

    upload Exp 3

--- functions/classifyResponses.R
+++ functions/classifyResponses.R
@@ -27,6 +27,41 @@ classifyRes <-function (
       CorrectNew = ifelse(updPos=="yes", Correct, NA),
       OutSame = ifelse(updPos=="yes", OutSame, NA)
     )
+
+  return (results)
+}
+
+
+classifyRes2 <-function (
+    .data,
+    initial = initial_item,
+    final = final_item,
+    npl_trained = npl_trained,
+    npl_untrained = npl_untrained,
+    answer = answer,
+    updPos = updPos,
+    groupLevel = c(participant, block, trial)) {
+  
+  results <- .data %>% 
+    group_by(across({{groupLevel}})) %>% 
+    mutate(response = case_when(
+      {{final}}=={{answer}} ~ "Correct",
+      {{answer}} %in% {{final}}[{{updPos}}=="yes"] & {{answer}}!="#" ~ "TransNew",
+      {{answer}} %in% {{final}}[{{updPos}}=="no"] & {{answer}}!="#" ~ "TransOld",
+      {{answer}}=={{initial}} & {{answer}}!="#" & {{updPos}}=="yes" ~ "OutSame",
+      {{answer}} %in% {{initial}} & {{answer}}!="#" & {{updPos}}=="no" ~ "OutOther",
+      stringr::str_detect({{npl_trained}},{{answer}}) ~ "NPLtrained",
+      stringr::str_detect({{npl_untrained}},{{answer}}) ~ "NPLuntrained",
+      TRUE ~ "NoRes"
+    )) %>% 
+    ungroup() %>% 
+    mutate(value = 1) %>% 
+    pivot_wider(names_from = response, values_from = value, values_fill = 0) %>% 
+    mutate(
+      CorrectOld = ifelse(updPos=="no", Correct, NA),
+      CorrectNew = ifelse(updPos=="yes", Correct, NA),
+      OutSame = ifelse(updPos=="yes", OutSame, NA)
+    )
   
   return (results)
 }
--- scripts/schema.R
+++ scripts/schema.R
@@ -15,6 +15,7 @@ ac_fun <- function(t, intercept, slope) {
   
 }
 
+library(tidyverse)
 
 Plot_r <- ggplot2::ggplot()+
   ggplot2::geom_function(fun = r_fun, args = list(intercept = -1, slope = 1), xlim = c(0, 4))+
@@ -38,13 +39,15 @@ ggplot2::ggsave(
   
 
 Plot_ac <- ggplot2::ggplot()+
-  ggplot2::geom_function(fun = ac_fun, args = list(intercept = 3, slope = 0.2), xlim = c(0, 4))+
-  ggplot2::geom_function(fun = ac_fun, args = list(intercept = 3, slope = 0.5), xlim = c(0, 4))+
+  ggplot2::geom_function(fun = ac_fun, args = list(intercept = 3.5, slope = 0.15), xlim = c(0, 4))+
+  ggplot2::geom_function(fun = ac_fun, args = list(intercept = 3.5, slope = 0.3), xlim = c(0, 4))+
+  ggplot2::geom_function(fun = ac_fun, args = list(intercept = 3.5, slope = -0.2), xlim = c(0, 4))+
   ylim(2.5,5)+
   labs(
     x = "Block",
     title = "Memory parameter"
   )+
+  theme_bw()+
   theme(
     axis.title.y = ggplot2::element_blank(),
     axis.text = ggplot2::element_text(size = 10.5),
@@ -54,5 +57,36 @@ ggplot2::ggsave(
   plot = Plot_ac,
   path = "./figures/figures-Exp2/",
   filename = "Plot_Exp2_schema_ac.pdf",
+  width = 6, height = 4
+)
+
+# Item memory and familiarity
+
+Plot_familiarity <- ggplot2::ggplot()+
+  ggplot2::geom_function(
+    fun = ac_fun,
+    args = list(intercept = 1, slope = 0.4),
+    aes(linetype = "familiarity"))+
+  ggplot2::geom_function(
+    fun = ac_fun,
+    args = list(intercept = 2, slope = 0.2),
+    aes(linetype = "activation"))+
+  scale_linetype_manual(
+    values = c("familiarity" = "solid", "activation" = "dashed"))+
+  xlim(0, 4)+
+  ylim(0,3)+
+  labs(
+    x = "Block",
+  )+
+  theme_bw()+
+  theme(
+    axis.title.y = ggplot2::element_blank(),
+    axis.text = ggplot2::element_text(size = 10.5),
+  )
+
+ggplot2::ggsave(
+  plot = Plot_familiarity,
+  path = "./figures/figures-Exp2/",
+  filename = "Plot_Exp2_schema_familiarity.pdf",
   width = 6, height = 4
 )
